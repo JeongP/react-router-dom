0ca395443c3c6f5d9840652f08ccc5611f1509ba
JeongPyo <jeongpyo_park@tmax.co.kr>
2020-11-10 11:14:40 +0900

    route using Switch & not found 처리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowerRouter as Router, Route} from 'react-router-dom'
+import {BrowerRouter as Router, Route, Switch} from 'react-router-dom'
 
 function Home () {
   return (
@@ -37,9 +37,14 @@ function App() {
       <li><a href="/">Home</a></li>
       <li><a href="/topics">Topics</a></li>
       <li><a href="/contact">Contact</a></li>
-      <Route exact path="/"><Home></Home></Route>
-      <Route path="/topics"><Topics></Topics></Route>
-      <Route path="/contact"><Contact></Contact></Route>
+
+      {/* Switch는 하나만 선택하고 나머지 것들은 버린다. */}
+      <Switch>
+        <Route exact path="/"><Home></Home></Route>
+        <Route path="/topics"><Topics></Topics></Route>
+        <Route path="/contact"><Contact></Contact></Route>
+        <Route path="/">Not Found</Route>
+      </Switch>
     </div>
   );
 }
