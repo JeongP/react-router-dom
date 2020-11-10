b7b7b01a095e4b1a8ac747d2cdbfe71ef74fee84
JeongPyo <jeongpyo_park@tmax.co.kr>
2020-11-10 10:43:33 +0900

    setting proj & Main router

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowerRouter as Router} from 'react-router-dom'
+
+function Home () {
+  return (
+    <div>
+      <h2>Home</h2>
+      home...
+    </div>
+    
+  );
+}
+
+function Topics () {
+  return(
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  );
+}
+
+function Contact() {
+  return(
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React Router DOM</h1>
+      
+    </div>
+  );
+}
+
+export default App;
